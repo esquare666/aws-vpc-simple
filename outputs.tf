5d10a58802fd318b481abaf72083c816ac5cbaaa
esquare666 <elayaraja.kv@gmail.com>
2020-05-20 10:38:17 +1200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
 output "vpc_id" {
   description = "The ID of the VPC"
-  value       = aws_vpc.id
+  value       = aws_vpc.esquare.id
 }
 
 output "vpc_arn" {
   description = "The ARN of the VPC"
-  value       = aws_vpc.arn
+  value       = aws_vpc.esquare.arn
 }
 
 output "vpc_cidr_block" {
   description = "The CIDR block of the VPC"
-  value       = aws_vpc.cidr_block
+  value       = aws_vpc.esquare.cidr_block
 }
 
 output "default_security_group_id" {
   description = "The ID of the security group created by default on VPC creation"
-  value       = aws_vpc.default_security_group_id
+  value       = aws_vpc.esquare.default_security_group_id
 }
